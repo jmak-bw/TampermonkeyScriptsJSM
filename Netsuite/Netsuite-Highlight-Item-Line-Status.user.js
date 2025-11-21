e0fc4dfd0d60f5c592ec093b44afa25825619ac8
jmak-bw <jmak@babcock-wanson.com>
2025-11-21 14:45:43 +0000

    Update Netsuite-Highlight-Item-Line-Status.user.js

--- Netsuite/Netsuite-Highlight-Item-Line-Status.user.js
+++ Netsuite/Netsuite-Highlight-Item-Line-Status.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Netsuite] Visual aid for items on transaction page
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.1.2
 // @description  Highlight rows based on Back Ordered and Fulfilled; highlight Invoiced column green if fully invoiced; skip rows with empty Available; show summary counts above table
 // @author       JSM
 // @match        https://*.app.netsuite.com/app/accounting/transactions/salesord.nl?id=*
