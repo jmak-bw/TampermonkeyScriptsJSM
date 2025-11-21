c76a9fe07ff4245ec4028b4811ebe74c77ba07e0
jmak-bw <jmak@babcock-wanson.com>
2025-11-21 14:35:35 +0000

    Create Netsuite-Better-Tab-Titles.user.js

---
+++ Netsuite/Netsuite-Better-Tab-Titles.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         [Netsuite] Better Tab Titles
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Set custom title for Netsuite Sales Orders
+// @match        https://*.app.netsuite.com/app/accounting/transactions/*
+// @run-at       document-start
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/jmak-bw/TampermonkeyScriptsJSM/main/Netsuite/Netsuite-Better-Tab-Titles.user.js
+// @downloadURL  https://raw.githubusercontent.com/jmak-bw/TampermonkeyScriptsJSM/main/Netsuite/Netsuite-Better-Tab-Titles.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const interval = setInterval(() => {
+        const soDiv = document.querySelector('div.uir-record-id');
+        const statusDiv = document.querySelector('div.uir-record-status');
+
+        if (soDiv && statusDiv) {
+            let soText = soDiv.textContent.trim();
+            let statusText = statusDiv.textContent.trim();
+
+            // Remove the unwanted part
+            soText = soText.replace(/BWUK-2025-/, '');
+
+            // Set as page title
+            document.title = `${soText} - ${statusText}`;
+
+            clearInterval(interval); // Stop checking once found
+        }
+    }, 500); // Check every 500ms
+})();
